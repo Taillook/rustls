093653583bf95dea58fdb49fbda29117da6e865e
Taillook <jousoku@gmail.com>
2021-02-14 20:20:58 +0900

    add comanndline argment

--- rustls/src/main.rs
+++ rustls/src/main.rs
@@ -1,7 +1,14 @@
+use std::path::Path;
+use std::env;
 use std::fs;
 
 fn main() {
-    let paths = fs::read_dir("./").unwrap();
+    let target_path = match env::args().nth(1) {
+        Some(path) => path,
+        None => "./".to_string(),
+    };
+
+    let paths = fs::read_dir(Path::new(&target_path)).unwrap();
     for path in paths {
         println!("{}", path.unwrap().path().display())
     }
