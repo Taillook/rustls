4074b6afcf9bf2392585657a741f64c305fd6913
Taillook <jousoku@gmail.com>
2021-02-28 01:03:24 +0900

    add: printcol

--- rustls/src/main.rs
+++ rustls/src/main.rs
@@ -8,10 +8,9 @@ fn main() {
     };
 
     let mut dir_pathbufs = read_dir_sorted(target_path);
+
     dir_pathbufs = filter_invisible(dir_pathbufs);
-    for dir_pathbuf in dir_pathbufs {
-        println!("{}", dir_pathbuf.as_path().display())
-    }
+    printcol(&dir_pathbufs);
 }
 
 fn read_dir(target_path: String) -> Vec<PathBuf> {
@@ -68,5 +67,56 @@ fn window_size() -> Option<winsize> {
         None
     } else {
         Some(ws)
+    }
+}
+
+fn printcol(dir_pathbufs: &Vec<PathBuf>) {
+    if dir_pathbufs.len() == 0 {
+        return;
+    }
+    let maxsize: usize = match window_size() {
+        Some(ws) => ws.ws_col.into(),
+        None => 0,
+    };
+
+    let mut colwidth = 0;
+    for dir_pathbuf in dir_pathbufs {
+        let file_name_len = (dir_pathbuf
+            .file_name()
+            .unwrap()
+            .to_os_string()
+            .into_string()
+            .unwrap()
+            + "\t")
+            .len();
+        if colwidth < file_name_len {
+            colwidth = file_name_len;
+        }
+    }
+
+    let numcols = maxsize / colwidth - 1;
+
+    for (idx, dir_pathbuf) in dir_pathbufs.iter().enumerate() {
+        let file_name = match dir_pathbuf.file_name() {
+            Some(file_name) => file_name.to_os_string().into_string().unwrap(),
+            None => "".to_string(),
+        };
+        let file_name_len = file_name.len();
+
+        if file_name_len != 0 {
+            print!("{}", file_name);
+            for _ in 0..(colwidth - file_name_len) {
+                print!(" ");
+            }
+        }
+        if idx != 0 && (idx + 1) % numcols == 0 {
+            print!("\n");
+        } else {
+            print!("\t");
+        }
+    }
+
+    if numcols > dir_pathbufs.len() {
+        println!("");
     }
 }
