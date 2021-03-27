ddc2f7e9740628914c706e8dca9e87fa9895d246
Taillook <jousoku@gmail.com>
2021-03-27 12:36:31 +0900

    add: option `stream`

--- rustyls/src/main.rs
+++ rustyls/src/main.rs
@@ -15,6 +15,11 @@ fn main() {
                 .help("list one file per line")
                 .short("1"),
         )
+        .arg(
+            Arg::with_name("stream")
+                .help("fill width with a comma separated list of entries")
+                .short("m"),
+        )
         .arg(Arg::with_name("file").help("FILE").index(1));
     let matches = app.get_matches();
 
@@ -22,6 +27,8 @@ fn main() {
         colmuns(matches.value_of("file"));
     } else if matches.is_present("one") {
         one(matches.value_of("file"));
+    }  else if matches.is_present("stream") {
+        stream(matches.value_of("file"));
     } else {
         colmuns(matches.value_of("file"));
     }
@@ -66,6 +73,28 @@ fn one(file_name: Option<&str>) {
     if target_path.is_dir() {
         let pathbufs = filter_invisible(&read_dir_sorted(target_path));
         print::printscol(&pathbufs.unwrap());
+    } else {
+        println!("{}", target_path.to_str().unwrap());
+    }
+}
+
+fn stream(file_name: Option<&str>) {
+    let target_path_name = match file_name {
+        Some(path) => path.to_string(),
+        None => "./".to_string(),
+    };
+
+    let target_path = Path::new(&target_path_name);
+    if !target_path.exists() {
+        println!(
+            "rustls: {}: No such file or directory",
+            target_path.to_str().unwrap()
+        );
+        process::exit(1);
+    }
+    if target_path.is_dir() {
+        let pathbufs = filter_invisible(&read_dir_sorted(target_path));
+        print::printstream(&pathbufs.unwrap());
     } else {
         println!("{}", target_path.to_str().unwrap());
     }
